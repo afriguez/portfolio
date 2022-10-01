d5c7595bebaafea602569ac96c31b199ed7e3def
Fer <afriguez@gmail.com>
2022-09-30 20:54:08 -0500

    feat: add footer & icons

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { Spotify as LanyardSpotify } from "use-lanyard";
+import { Spotify } from "./Spotify";
+
+export const Footer = ({
+  spotify,
+}: {
+  spotify: LanyardSpotify | undefined | null;
+}) => {
+  return (
+    <footer className="flex flex-wrap items-center justify-center bg-primary py-4 px-10 sm:justify-between md:px-20">
+      <Spotify spotify={spotify} />
+      <p className="mt-5 text-[0.9rem] text-secondary sm:mt-0">
+        Copyright &copy; afriguez 2022. All rights reserved
+      </p>
+    </footer>
+  );
+};
