9c67fb2fa868f43876b557a64fe9609ff08a51b4
Fer <afriguez@gmail.com>
2022-10-15 13:47:15 -0500

    feat: update spotify image

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -6,15 +6,17 @@ export const Spotify = ({
   spotify: lanyardSpotify | undefined | null;
 }) => {
   return spotify ? (
-    <div className="flex h-[8rem] max-w-[25rem] justify-center items-center rounded-lg bg-[#1F1F1F] p-5">
+    <div className="flex h-[8rem] max-w-[25rem] items-center justify-center rounded-lg bg-[#1F1F1F] p-5">
       <img
         src={spotify.album_art_url}
         alt="activity"
-        className="mr-3 h-[5.5rem] w-[5.5rem] rounded-lg"
+        className="mr-3 h-[5.5rem] w-[5.5rem] rounded-lg transition duration-500 hover:skew-y-6"
       />
-      <div className="flex flex-col text-secondary truncate">
-		<p className="font-bold text-[0.9rem] text-[#1ed760]">Listening to spotify...</p>
-        <p className="font-bold text-[0.8rem]">{spotify.song}</p>
+      <div className="flex flex-col truncate text-secondary">
+        <p className="text-[0.9rem] font-bold text-[#1ed760]">
+          Listening to spotify...
+        </p>
+        <p className="text-[0.8rem] font-bold">{spotify.song}</p>
         <p className="text-[0.8rem]">by {spotify.artist}</p>
         <p className="text-[0.8rem]">on {spotify.album}</p>
       </div>
