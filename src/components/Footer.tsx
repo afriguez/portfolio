2dbddcbbb5fcb6e8fdb800d91e6f84d6007b12d0
Fer L <afriguez@gmail.com>
2023-01-10 12:00:03 -0500

    feat: remove footer background

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ export const Footer = ({
   spotify: LanyardSpotify | undefined | null;
 }) => {
   return (
-    <footer className="flex flex-wrap items-center justify-center bg-primary py-4 px-10 sm:justify-between md:px-20">
+    <footer className="flex flex-wrap items-center justify-center py-4 px-10 sm:justify-between md:px-20">
       <Spotify spotify={spotify} />
       <p className="mt-5 text-[0.9rem] text-secondary sm:mt-0">
-        Copyright &copy; afriguez 2022. All rights reserved
+        Copyright &copy; afriguez {new Date().getFullYear()}. All rights reserved
       </p>
     </footer>
   );
