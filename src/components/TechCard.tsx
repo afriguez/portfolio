61df273734f2558a54fc7063f858c222d03ac4ad
Fer L <afriguez@gmail.com>
2023-01-12 13:18:11 -0500

    feat: add react-tooltip pkg

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -1,4 +1,6 @@
 import { ReactElement } from "react";
+import { Tooltip } from "react-tooltip";
+import 'react-tooltip/dist/react-tooltip.css';
 
 export const TechCard = ({
   name,
@@ -14,18 +16,30 @@ export const TechCard = ({
   url: string;
 }) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
-      <div className="border-3 flex items-stretch overflow-hidden rounded-lg border border-secondary outline-none transition-all  hover:-translate-y-1 active:translate-y-1">
-        <div
-          className="flex items-center justify-center p-5"
-          style={{ backgroundColor: color }}
-        >
-          <div className="">{icon}</div>
+	<div>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <div className="border-3 flex items-stretch overflow-hidden rounded-lg border border-secondary outline-none transition-all  hover:-translate-y-1 active:translate-y-1">
+          <div
+            className="flex items-center justify-center p-5"
+			id={`id-${name}`} data-tooltip-content={name} data-tooltip-variant="dark"
+			data-tooltip-float={true}
+            style={{ backgroundColor: color }}
+          >
+            <div className="">{icon}</div>
+          </div>
+          <p
+			className="mx-[10px] self-center p-2 text-secondary"
+			id={`id-${name}-out`}
+			data-tooltip-content={name}
+			data-tooltip-variant="dark"
+			data-tooltip-place="top"
+		  >
+            {desc}
+          </p>
         </div>
-        <p className="mx-[10px] self-center p-2 text-secondary">
-          {desc}
-        </p>
-      </div>
-    </a>
+      </a>
+      <Tooltip anchorId={`id-${name}`} />
+      <Tooltip anchorId={`id-${name}-out`} />
+	</div>
   );
 };
