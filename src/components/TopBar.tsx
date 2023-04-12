75ee3bf930735fc1f5ac8608b26757272f99a2c8
Fer L <afriguez@gmail.com>
2023-04-12 16:23:24 -0500

    feat: update topbar responsiveness

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -8,7 +8,7 @@ import {
 
 export const TopBar = ({discordUser}: {discordUser: DiscordUser | undefined}) => {
   return (
-    <div className="flex flex-wrap items-center justify-center bg-primary py-5 px-10 shadow-md shadow-[#000000] sm:justify-between md:px-20">
+    <div className="flex flex-wrap items-center justify-center bg-primary py-5 px-10 shadow-md shadow-[#000000] flex-col sm:flex-row sm:justify-between md:px-20">
       <div className="flex w-[12.5rem] justify-around">
         <a
           href="https://discord.com/users/361159575036231691"
@@ -39,15 +39,15 @@ export const TopBar = ({discordUser}: {discordUser: DiscordUser | undefined}) =>
           <TwitterIcon />
         </a>
       </div>
-      <div className="flex justify-center items-center gap-5">
-		<a href="https://blog.afriguez.com" className="text-secondary hover:text-gray">
+      <div className="flex justify-center items-center sm:mt-0 mt-7 gap-5">
+        <a href="https://blog.afriguez.com" className="text-secondary hover:text-gray">
           <p className="text-[2rem] font-bold">Blog</p>
         </a>
-		<img
-		  src={discordUser ? `https://cdn.discordapp.com/avatars/${discordUser!.id}/${discordUser!.avatar}` : "https://github.com/afriguez.png"}
-		  alt="avatar"
-		  className="h-[3.5rem] w-[3.5rem] rounded-lg"
-		/>
+        <img
+          src={discordUser ? `https://cdn.discordapp.com/avatars/${discordUser!.id}/${discordUser!.avatar}` : "https://github.com/afriguez.png"}
+          alt="avatar"
+          className="h-[3.5rem] w-[3.5rem] rounded-lg"
+        />
       </div>
     </div>
   );
