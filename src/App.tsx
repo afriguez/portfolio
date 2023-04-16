d6c79bc78ad879e88b882959a0bbd823eb7a66e4
Fer L <afriguez@gmail.com>
2023-04-16 10:12:23 -0500

    style: formatting

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,17 @@ export const App = () => {
   const lanyard = useLanyardWS(DISCORD_ID);
 
   return (
-      <div className="flex h-screen flex-col justify-between">
-		  <TopBar discordUser={lanyard?.discord_user}/>
-        <div className="md:px-30 primary flex flex-wrap justify-around px-10 py-10">
-          <About />
-          <div className="flex flex-col items-center justify-center">
-            <Discord lanyard={lanyard} />
-            <Projects />
-          </div>
+    <div className="flex h-screen flex-col justify-between">
+      <TopBar discordUser={lanyard?.discord_user} />
+      <div className="md:px-30 primary flex flex-wrap justify-around px-10 py-10">
+        <About />
+        <div className="flex flex-col items-center justify-center">
+          <Discord lanyard={lanyard} />
+          <Projects />
         </div>
-        <Footer spotify={lanyard?.spotify} />
-      <Background />
       </div>
+      <Footer spotify={lanyard?.spotify} />
+      <Background />
+    </div>
   );
 };
