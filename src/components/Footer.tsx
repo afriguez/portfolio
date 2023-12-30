0d533458c2cae7fd2013096f69f13dbd061dc763
Fer L. <afriguez@gmail.com>
2023-12-30 08:30:18 -0500

    feat: add footer links and change content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,8 @@ export const Footer = ({
 }: {
   lanyard: Data | undefined
 }) => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="flex flex-wrap items-center justify-center py-3 px-10 lg:justify-between md:px-20">
       {lanyard?.spotify ? (
@@ -15,8 +17,13 @@ export const Footer = ({
         <YoutubeMusic lanyard={lanyard} />
       }
       <p className="mt-5 text-[0.9rem] text-secondary text-center sm:mt-0 px-4 md:p-0">
-        Copyright &copy; afriguez {new Date().getFullYear()}. All rights reserved
+        Fer L. {year} • <Link url="https://afriguez.com" text="afriguez.com" />&nbsp;&gt;&nbsp;
+        <Link url="https://github.com/afriguez/portfolio/blob/main/LICENSE.md" text="MIT License" />
       </p>
     </footer>
   );
 };
+
+const Link = ({ url, text }: { url: string, text: string }) => {
+  return <a href={url} className="hover:underline hover:underline-offset-2">{text}</a>
+}
